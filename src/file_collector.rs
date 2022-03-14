c7b9974f8530992aaa94675b3d18b384ef85a80b
dk949 <56653556+dk949@users.noreply.github.com>
2022-03-14 09:39:42 +0000

    implemented file collector

--- src/file_collector.rs
+++ src/file_collector.rs
@@ -1,24 +1,27 @@
 use crate::types::ProjResult;
-use std::convert::identity;
 use std::path::Path;
 use walkdir::WalkDir;
 
-pub fn run(dir: &String) -> ProjResult<Vec<String>> {
+#[derive(Debug)]
+pub enum FileExtension {
+    Extension(String),
+    Path(String),
+}
+
+pub fn run(dir: &String) -> ProjResult<Vec<FileExtension>> {
     if Path::new(dir).exists() {
         Ok(WalkDir::new(dir)
             .into_iter()
             .filter_map(Result::ok)
             .filter(|e| e.file_type().is_file())
             .map(|e| {
-                Some(
-                    e.file_name()
-                        .to_string_lossy()
-                        .split('.')
-                        .last()?
-                        .to_string(),
-                )
+                let p = e.path();
+                if let Some(ext) = p.extension().map(|f| f.to_string_lossy().to_string()) {
+                    FileExtension::Extension(ext)
+                } else {
+                    FileExtension::Path(p.to_string_lossy().to_string())
+                }
             })
-            .filter_map(identity)
             .collect())
     } else {
         Err("provided directory does not exist".to_string())
--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,4 @@
+use crate::file_collector::FileExtension;
 pub type ProjError = String;
 pub type ProjResult<T> = Result<T, ProjError>;
-pub type Files = Vec<String>;
+pub type Files = Vec<FileExtension>;
