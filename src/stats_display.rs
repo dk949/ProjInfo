c0e43b99ea0f47bab3dbfb439100c687dc9c0fa3
dk949 <56653556+dk949@users.noreply.github.com>
2022-03-15 10:57:41 +0000

    added mechanism to displaystats

--- src/stats_display.rs
+++ src/stats_display.rs
@@ -2,6 +2,26 @@ use crate::file_analizer::Stats;
 use crate::types::*;
 
 pub fn print(stats: Stats) -> ProjResult<()> {
-    println!("{:?}", stats);
+    const MAX_WITH: usize = 50;
+    const FILL: &str = "─";
+    const SPACE: &str = " ";
+    let title = " Summary ";
+    let total_count = stats.total.to_string();
+    println!("╭─{title:─<titleWidth$}─╮", titleWidth = MAX_WITH);
+    for (language, count) in stats.langs.iter() {
+        let percentage = format!("{:.2}%", ((*count as f32) / (stats.total as f32)) * 100.);
+        println!(
+            "│ {language}: {percentage: <langWidth$}│",
+            langWidth = MAX_WITH - (language.len()) - 1
+        );
+    }
+
+    println!("│ {SPACE: <spacerWidth$} │", spacerWidth = MAX_WITH);
+    println!(
+        "│ Total files: {total_count: <langWidth$}│",
+        langWidth = MAX_WITH - ("Total files").len() - 1
+    );
+    println!("╰─{FILL:─<footerWidth$}─╯", footerWidth = MAX_WITH);
+
     Ok(())
 }
