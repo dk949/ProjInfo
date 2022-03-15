f599b80462abf423e667510c4cb57dc1b85e15c9
dk949 <56653556+dk949@users.noreply.github.com>
2022-03-15 10:57:26 +0000

    skipping hidden directories in file collector

--- src/file_collector.rs
+++ src/file_collector.rs
@@ -1,6 +1,8 @@
+use crate::arg_parser::Args;
 use crate::types::ProjResult;
+use std::fs::canonicalize;
 use std::path::Path;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 
 #[derive(Debug)]
 pub enum FileExtension {
@@ -8,10 +10,20 @@ pub enum FileExtension {
     Path(String),
 }
 
-pub fn run(dir: &String) -> ProjResult<Vec<FileExtension>> {
-    if Path::new(dir).exists() {
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with('.'))
+        .unwrap_or(false)
+}
+
+pub fn run(args: &Args) -> ProjResult<Vec<FileExtension>> {
+    let dir = canonicalize(Path::new(&args.dir)).or(Err("Could not normalize path"))?;
+    if dir.exists() {
         Ok(WalkDir::new(dir)
             .into_iter()
+            .filter_entry(|e| args.no_skip_dots || !is_hidden(e))
             .filter_map(Result::ok)
             .filter(|e| e.file_type().is_file())
             .map(|e| {
